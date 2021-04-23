c2a7974bfd2814fcdce5b8f727bdcf231309555a
dylanbury <58638255+dylanbury@users.noreply.github.com>
2021-04-22 18:53:14 -0700

    edited Read.md, edited front-end

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AutoIntegrationTester
+# axon
+## Automatically generate integration testing files based on web API endpoints.
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,7 +4,7 @@ import './app.scss';
 const App = () => {
   return (
     <div className="app">
-      <h1>  Eon App!!</h1>
+      <h1>Electron App!!</h1>
     </div>
   );
 }
