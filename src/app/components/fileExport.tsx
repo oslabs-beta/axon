8686d408f6b659521cf56708fcf353cf5691d8da
dylanbury <58638255+dylanbury@users.noreply.github.com>
2021-04-28 20:13:38 -0700

    add state change for text box and added comments
    Co-authored-by: Michael Caballero <caballeromichaelus@gmail.com>
    Co-authored-by: Dylan Bury <dsbury91@yahoo.com>
    Co-authored-by: Sebastian Damazo <ernesto01@ucsb.edu>
    Co-authored-by: Ali Ziel <aliziel9@gmail.com>

--- src/app/components/fileExport.tsx
+++ src/app/components/fileExport.tsx
@@ -1,15 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import check from '../../../assets/Checkmark.png'
 
+
+// functional component to create export functionality   
 const FileExport = (props:any) => {
+
+
+  // global variable to store prop that carries returned supertest from main algorithm
+  let text = props.superTest;
+  // global variable to hold exported file name
+  let filename:any;
+ // conditional statement to check if user left input field blank
+  if(props.textInput === ""){
+      // if input field empty filename is supertest.js
+      filename = "superTest.js"
+    }else{
+      // exported file gets saved as inputed filename
+      filename = props.textInput + ".js";
+    }
+
+  // function that gets called after invoking export button to change current state
   const onFileDownload = ():void => {
     
-    return props.setProgressState("Supertest sucessfully exported", 3, check);    
+    // return passed in prop to reset state to passed in arguments
+    return props.setProgressState("Supertest sucessfully exported", "100%", check);    
   }
 
-  let text = props.superTest;
-  let filename = "superTest.js";
-
+  // create function to create a tag that will encode supertext string into a dowloadable file
   function download(file:string, text:string) {
               
     //creating an invisible element
@@ -26,16 +43,18 @@ const FileExport = (props:any) => {
     element.click();
   
     document.body.removeChild(element);
+    // call fuction to reset state on root file
     onFileDownload();
 }
   
-
+  // return input tag to root file that holds the export button functionality
   return (
     <div>
-      <input type="button" id="exportbtn" value="Export" onClick={() => {
-        download(filename, text)
-        
+
+      <input disabled={props.disableStatus} type="button" id="exportbtn" value="Export" onClick={() => {
+        download(filename, text)   
       }}/>
+
     </div>
   )
 };
