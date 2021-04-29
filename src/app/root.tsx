195375682f5f63b9f378dc6ee9eb3d0b2f01f65b
dylanbury <58638255+dylanbury@users.noreply.github.com>
2021-04-28 20:24:41 -0700

    update logo, add disable state, add comments
    Co-authored-by: Michael Caballero <caballeromichaelus@gmail.com>
    Co-authored-by: Dylan Bury <dsbury91@yahoo.com>
    Co-authored-by: Sebastian Damazo <ernesto01@ucsb.edu>
    Co-authored-by: Ali Ziel <aliziel9@gmail.com>

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './app.scss';
 import FileImport from '@/app/components/fileImport';
 import FileExport from '@/app/components/fileExport';
-import logo from '../../assets/AxonLogoSize.png';
+import logo from '../../assets/AxonLogoDotSize.png';
 import uploadFolder from '../../assets/folderImport.png';
 import createdTest from '../../assets/codeicon.png';
 
@@ -10,12 +10,16 @@ import createdTest from '../../assets/codeicon.png';
 // import algo1 from @app...
 
 const RootComponent = () => {
+  // Using Hooks to set our state
   const [axonState, setAxonState] = useState <any | undefined> ({
     fileList: {},
     superTestCode: "",
     progressText: 'Import server folder here',
     image: uploadFolder,
-    progressVal: 0
+    progressVal: "0%",
+    createDisable: true,
+    exportDisable: true,
+    textInput: '',
     
   });
 
@@ -28,32 +32,47 @@ const RootComponent = () => {
   
   return (
     <main>
+      {/* Logo at top of the page */}
       <header>
         <img id="axonTitle" src={logo}></img>
       </header>
        
+       {/* Main body (currently middle, will be left side) */}
       <div className="contentBox">
         
+        {/* First part of the body. Displayed Progress Text and Bar */}
         <div id="progressDisplay">
           <h1>{axonState.progressText}</h1>
-          <progress id="progressBar" value={`${axonState.progressVal}`} max="3"> nothing here </progress>
+          <div id="progressbarNew" >
+            <div style={{width: `${axonState.progressVal}`}}></div>
+          </div>
         </div>
-    
+        
+        {/* Second part of the body. Includes Import Button and Image. */}
         <div id="import">
-          <FileImport setFileState={(newFileState: any, newProgressState:any, newImage: any, progVal: any) => setAxonState({...axonState, fileList: newFileState, progressText: newProgressState, image: newImage, progressVal: progVal}) }/> 
+          <FileImport setFileState={(newFileState: any, newProgressState:any, newImage: any, progVal: any, boolCreate:boolean, boolExport:boolean, resetInput: string) => 
+            setAxonState({...axonState, fileList: newFileState, progressText: newProgressState, image: newImage, progressVal: progVal, createDisable:boolCreate, exportDisable: boolExport, textInput: resetInput})  }  /> 
           <img id="fileImg" src={axonState.image}></img>
         </div>
 
-        <div id="createExport">
-          <button id="createButton" onClick={() => setAxonState({...axonState, superTestCode: /* algoname */createSupertest(axonState.fileList), progressText: "Supertest files created", image:createdTest, progressVal: 2 })} >Create</button> 
-          {/* </div> */}
-          <h1> {console.log(axonState.superTestCode)}</h1>
-          {/* <div id="exportbtn"> */}
-          <FileExport superTest={axonState.superTestCode} setProgressState={(newProgressState:any, progVal: any, checkImg: any) => setAxonState({...axonState, progressText: newProgressState, progressVal: progVal, image: checkImg}) }/>
+        {/* Last part of the body. Displays Create &Export Button, and file name text box */}
+        <div id="bottomBox">
+          
+          {/* Displays Text box when File is created and ready for export */}
+          <div>
+            <input hidden={axonState.exportDisable} placeholder="Enter Your File Name" type="text" id="fname" name="fname" value={axonState.textInput} onChange={e => {setAxonState({...axonState, textInput: e.target.value});}}/>
+          </div>
+          
+          {/* Displays Create Button and Export Button. */}
+          <div id="createExport">
+            <button disabled={axonState.createDisable} id="createButton" onClick={() => setAxonState({...axonState, superTestCode: /* algoname */createSupertest(axonState.fileList), progressText: "Supertest files created", image:createdTest, progressVal: "66.66%", exportDisable: false, createDisable: true })} >Create</button> 
+            {/* {console.log(axonState.filelist)} */}
+            <FileExport superTest={axonState.superTestCode} textInput={axonState.textInput} disableStatus={axonState.exportDisable} setProgressState={(newProgressState:any, progVal: any, checkImg: any) => 
+              setAxonState({...axonState, progressText: newProgressState, progressVal: progVal, image: checkImg}) }/>
+          </div>
         </div>
 
       </div>
-
     </main>
   )
 }
